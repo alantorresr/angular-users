cc66d8f7badef34d45ace60617c2e8405f2e1826
Alan Torres <alan9805@gmail.com>
2020-06-13 11:21:37 -0500

    users api endpoint and service added

--- src/app/services/usersapi.service.ts
+++ src/app/services/usersapi.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const URL_API = environment.API.EndPoint.Users;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersapiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserById(id, token) {
+
+    const headers = new HttpHeaders({
+      'x-access-token': token
+    });
+
+    const data: any = {
+      "id": id
+    };
+
+    let result: any;
+    return this.http.post(`${URL_API}/getbyid`, data, {headers});
+    // return result.catalogoAño;
+  }
+
 }
