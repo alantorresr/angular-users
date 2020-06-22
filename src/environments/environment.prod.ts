40f7abdb7270b7435937887224f740db7fe07e45
Alan Torres <alan9805@gmail.com>
2020-06-21 20:29:01 -0500

    production env added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  API: {
+    EndPoint: {
+      NorthWind: 'http://api.northwind.com/v1/Analysis/Northwind/',
+      Users: 'http://api.auth.com/v1'
+    }
+  }
 };
